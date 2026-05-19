9c8c5929c8dc57a6ef4f7815e2ef7adcde59f8f5
Kark Angelo <karkangelovaronapada@gmail.com>
2026-05-19 10:42:26 +0800

    feat: update sitemap configuration to exclude Google verification file

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import sitemap from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: "https://neurex-documentation.vercel.app"
+      hostname: "https://neurex-documentation.vercel.app",
+      // Add the exclude array to ignore the verification file
+      exclude: ['/googlef9813abd757c1198'] 
     })
   ],
 })
